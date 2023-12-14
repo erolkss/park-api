19e62b6914ed1243e32ca89fe208f8e70e434ebc
Lucas Lima <lucaserolima@gmail.com>
2023-12-14 19:26:57 -0400

    Firs Commit

---
+++ src/main/java/br/com/ero/demoparkapi/config/SpringTimeZoneConfig.java
@@ -0,0 +1,14 @@
+package br.com.ero.demoparkapi.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimeZoneConfig {
+    @PostConstruct
+    public void timeZoneConfig(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Manaus"));
+    }
+}
