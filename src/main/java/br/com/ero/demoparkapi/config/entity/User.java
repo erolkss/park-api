5bf17bc83951b04edf3b6005aa6c670d2afbbca1
Lucas Lima <lucaserolima@gmail.com>
2023-12-14 20:41:30 -0400

    Created the User entity

---
+++ src/main/java/br/com/ero/demoparkapi/config/entity/User.java
@@ -0,0 +1,57 @@
+package br.com.ero.demoparkapi.config.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+    @Column(name = "name", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 200)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+    @Column(name = "create_by")
+    private String createBy;
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    public enum Role {
+        ROLE_ADMIN,
+        ROLE_CLIENT
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                '}';
+    }
+}
