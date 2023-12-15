06e1fef4c831072a9b9da3e14de6b24384de9f62
Lucas Lima <lucaserolima@gmail.com>
2023-12-14 20:55:20 -0400

    Creating the method to save user in the UserController Class.

--- src/main/java/br/com/ero/demoparkapi/service/UserService.java
+++ src/main/java/br/com/ero/demoparkapi/service/UserService.java
@@ -1,6 +1,8 @@
 package br.com.ero.demoparkapi.service;
 
+import br.com.ero.demoparkapi.config.entity.User;
 import br.com.ero.demoparkapi.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,8 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/br/com/ero/demoparkapi/web/controller/UserController.java
+++ src/main/java/br/com/ero/demoparkapi/web/controller/UserController.java
@@ -1,13 +1,25 @@
 package br.com.ero.demoparkapi.web.controller;
 
+import br.com.ero.demoparkapi.config.entity.User;
 import br.com.ero.demoparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User user1 = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user1);
+    }
 
 }
