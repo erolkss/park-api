0b3bd646e9f8ae3d1e15f1ab73d2fcd00883800d
Lucas Lima <lucaserolima@gmail.com>
2023-12-17 21:08:09 -0400

    adding jakarta validation dependency

--- src/main/java/br/com/ero/demoparkapi/web/dto/UserCreateDto.java
+++ src/main/java/br/com/ero/demoparkapi/web/dto/UserCreateDto.java
@@ -1,11 +1,12 @@
 package br.com.ero.demoparkapi.web.dto;
 
 
+import jakarta.validation.constraints.Email;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UserCreateDto {
-
+    @Email(regexp = "", message = "Invalid email format")
     private String username;
     private String password;
 
