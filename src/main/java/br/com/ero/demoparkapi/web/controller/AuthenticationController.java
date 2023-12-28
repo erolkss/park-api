d302635f64992d0ed0f1558c47822cf2f1e396e1
Lucas Lima <lucaserolima@gmail.com>
2023-12-27 21:14:49 -0400

    AuthenticationController class created so that the user's credentials are sent via the Post method. - restore

---
+++ src/main/java/br/com/ero/demoparkapi/web/controller/AuthenticationController.java
@@ -0,0 +1,46 @@
+package br.com.ero.demoparkapi.web.controller;
+
+import br.com.ero.demoparkapi.jwt.JwtToken;
+import br.com.ero.demoparkapi.jwt.JwtUserDetailsService;
+import br.com.ero.demoparkapi.web.dto.UserLoginDto;
+import br.com.ero.demoparkapi.web.exception.ErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/v1")
+public class AuthenticationController {
+    private final JwtUserDetailsService detailsService;
+    private final AuthenticationManager authenticationManager;
+
+    public ResponseEntity<?> authenticate(@RequestBody @Valid UserLoginDto dto, HttpServletRequest request) {
+        log.info("Authentication process from login {}", dto. getUsername());
+
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
+
+            authenticationManager.authenticate(authenticationToken);
+            JwtToken token = detailsService.getTokenAuthenticated(dto.getUsername());
+            return ResponseEntity.ok(token);
+
+        }catch (AuthenticationException ex){
+        log.warn("Bad Credentials from username '{}'", dto.getUsername());
+        }
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Invalid Credentials"));
+    }
+}
