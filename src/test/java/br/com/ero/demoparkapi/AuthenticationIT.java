9ce2572ffb99a80cde18a437e1572d5424683b07
Lucas Lima <lucaserolima@gmail.com>
2024-01-06 20:34:16 -0400

    Added Test - Authenticate_WithInvalidUsername_ReturnErrorMessageHttpStatus422()

--- src/test/java/br/com/ero/demoparkapi/AuthenticationIT.java
+++ src/test/java/br/com/ero/demoparkapi/AuthenticationIT.java
@@ -62,4 +62,33 @@ public class AuthenticationIT {
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(400);
 
     }
+
+    @Test
+    public void Authenticate_WithInvalidUsername_ReturnErrorMessageHttpStatus422(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserLoginDto("", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserLoginDto("@email.com", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
 }
