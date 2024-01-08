1e7ff6289cde4b419bcbcb02351c636316377f0e
Lucas Lima <lucaserolima@gmail.com>
2024-01-08 19:55:48 -0400

    Add ClientResponseDto e ClientMapper

---
+++ src/main/java/br/com/ero/demoparkapi/web/dto/mapper/ClientMapper.java
@@ -0,0 +1,20 @@
+package br.com.ero.demoparkapi.web.dto.mapper;
+
+
+import br.com.ero.demoparkapi.config.entity.Client;
+import br.com.ero.demoparkapi.web.dto.ClientCreateDto;
+import br.com.ero.demoparkapi.web.dto.ClientResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClientMapper {
+
+    private static Client toClient(ClientCreateDto dto){
+        return new ModelMapper().map(dto, Client.class);
+    }
+    private static ClientResponseDto toDto(Client client){
+        return new ModelMapper().map(client, ClientResponseDto.class);
+    }
+}
