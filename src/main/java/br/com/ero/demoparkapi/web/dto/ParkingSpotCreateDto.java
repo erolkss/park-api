c4fcca96e0d6390f18abbc362bbbc3bb3b105a80
Lucas Lima <lucaserolima@gmail.com>
2024-01-13 20:22:11 -0400

    Created Class the ParkingSpotDto

---
+++ src/main/java/br/com/ero/demoparkapi/web/dto/ParkingSpotCreateDto.java
@@ -0,0 +1,21 @@
+package br.com.ero.demoparkapi.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkingSpotCreateDto {
+    @NotBlank
+    @Size(min = 4, max = 4)
+    private String code;
+    @NotBlank
+    @Pattern(regexp = "FREE|BUSY")
+    private String status;
