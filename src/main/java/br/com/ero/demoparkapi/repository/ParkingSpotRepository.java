92b4f44afc94aef13de6ccf4c52636110f1042b8
Lucas Lima <lucaserolima@gmail.com>
2024-01-13 20:50:35 -0400

    Add method in ParkingSpotService - getByCode

--- src/main/java/br/com/ero/demoparkapi/repository/ParkingSpotRepository.java
+++ src/main/java/br/com/ero/demoparkapi/repository/ParkingSpotRepository.java
@@ -4,6 +4,10 @@ import br.com.ero.demoparkapi.config.entity.ParkingSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Long> {
+    Optional<ParkingSpot> findByCode(String code);
+
 }
--- src/main/java/br/com/ero/demoparkapi/service/ParkingSpotService.java
+++ src/main/java/br/com/ero/demoparkapi/service/ParkingSpotService.java
@@ -2,6 +2,7 @@ package br.com.ero.demoparkapi.service;
 
 import br.com.ero.demoparkapi.config.entity.ParkingSpot;
 import br.com.ero.demoparkapi.exception.CodeUniqueViolationException;
+import br.com.ero.demoparkapi.exception.EntityNotFoundException;
 import br.com.ero.demoparkapi.repository.ParkingSpotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -15,12 +16,19 @@ public class ParkingSpotService {
     private final ParkingSpotRepository parkingSpotRepository;
 
     @Transactional
-    public ParkingSpot saveParkingSpot(ParkingSpot parkingSpot) throws DataIntegrityViolationException{
+    public ParkingSpot saveParkingSpot(ParkingSpot parkingSpot) throws DataIntegrityViolationException {
         try {
             return parkingSpotRepository.save(parkingSpot);
         } catch (DataIntegrityViolationException exception) {
             throw new CodeUniqueViolationException(String.format("ParkingSpot with code '%s' already registered", parkingSpot.getCode()));
 
         }
+    }
+
+    @Transactional(readOnly = true)
+    public ParkingSpot getByCode(String code) {
+        return parkingSpotRepository.findByCode(code).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Parking Spot with Code '%s' Not Found", code))
+        );
     }
 }
