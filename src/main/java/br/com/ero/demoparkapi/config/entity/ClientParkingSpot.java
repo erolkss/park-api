410f4680ed606ad246ee7105d82430fdb38e59e0
Lucas Lima <lucaserolima@gmail.com>
2024-01-17 20:12:39 -0400

    Create Class Entity - ClientParkingSpot to relate Client and ParkingSpot

---
+++ src/main/java/br/com/ero/demoparkapi/config/entity/ClientParkingSpot.java
@@ -0,0 +1,81 @@
+package br.com.ero.demoparkapi.config.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients_have_parking_spot")
+@EntityListeners(AuditingEntityListener.class)
+public class ClientParkingSpot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "number_receipt", nullable = false, unique = true, length = 15)
+    private String receipt;
+    @Column(name = "plate", nullable = false, length = 8)
+    private String plate;
+    @Column(name = "brand", nullable = false, length = 45)
+    private String brand;
+    @Column(name = "model", nullable = false, length = 45)
+    private String model;
+    @Column(name = "color", nullable = false, length = 45)
+    private String color;
+    @Column(name = "entry_date", nullable = false)
+    private LocalDateTime  entryDate;
+    @Column(name = "exit_date")
+    private LocalDateTime exitDate;
+    @Column(name = "value", columnDefinition = "decimal(7,2)")
+    private BigDecimal value;
+    @Column(name = "discount", columnDefinition = "decimal(7,2)")
+    private BigDecimal discount;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client", nullable = false)
+    private Client client;
+    @ManyToOne
+    @JoinColumn(name = "id_parking_spot", nullable = false)
+    private ParkingSpot parkingSpot;
+
+
+    @CreatedDate
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+    @LastModifiedDate
+    @Column(name = "modification_date")
+    private LocalDateTime modificationDate;
+    @CreatedBy
+    @Column(name = "create_by")
+    private String createBy;
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClientParkingSpot that = (ClientParkingSpot) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
