3e9f5af2b4f8af94f9e826dbc76a38e2e5af4c3b
Lucas Lima <lucaserolima@gmail.com>
2024-01-18 20:52:34 -0400

    Add - package 'Util' and Class ParkingUtils

---
+++ src/main/java/br/com/ero/demoparkapi/util/ParkingUtils.java
@@ -0,0 +1,18 @@
+package br.com.ero.demoparkapi.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ParkingUtils {
+
+    public static String generateReceipt() {
+        LocalDateTime date = LocalDateTime.now();
+        String receipt = date.toString().substring(0, 19);
+        return receipt.replace("-", "")
+                .replace(":", "")
+                .replace("T", "-");
+    }
+}
