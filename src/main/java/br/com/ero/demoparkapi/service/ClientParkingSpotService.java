25824e85ca1b9f1cf0caf488d67f42a4c85d8cff
Lucas Lima <lucaserolima@gmail.com>
2024-01-18 20:58:45 -0400

    Add Method - saveClientParkingSpot() in Service

--- src/main/java/br/com/ero/demoparkapi/service/ClientParkingSpotService.java
+++ src/main/java/br/com/ero/demoparkapi/service/ClientParkingSpotService.java
@@ -1,11 +1,18 @@
 package br.com.ero.demoparkapi.service;
 
+import br.com.ero.demoparkapi.config.entity.ClientParkingSpot;
 import br.com.ero.demoparkapi.repository.ClientParkingSpotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ClientParkingSpotService {
     private final ClientParkingSpotRepository clientParkingSpotRepository;
+
+    @Transactional
+    public ClientParkingSpot saveClientParkingSpot(ClientParkingSpot clientParkingSpot){
+        return clientParkingSpotRepository.save(clientParkingSpot);
+    }
 }
