f47244bdc427b4e6de089c780c12c80e7eda20e0
Lucas Lima <lucaserolima@gmail.com>
2024-02-08 21:32:11 -0400

    Programming exercise 3: Calculating the discount amount

--- src/main/java/br/com/ero/demoparkapi/util/ParkingUtils.java
+++ src/main/java/br/com/ero/demoparkapi/util/ParkingUtils.java
@@ -3,7 +3,10 @@ package br.com.ero.demoparkapi.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ParkingUtils {
@@ -14,5 +17,37 @@ public class ParkingUtils {
         return receipt.replace("-", "")
                 .replace(":", "")
                 .replace("T", "-");
+    }
+
+    private static final double PRIMEIROS_15_MINUTES = 5.00;
+    private static final double PRIMEIROS_60_MINUTES = 9.25;
+    private static final double ADICIONAL_15_MINUTES = 1.75;
+
+
+    public static BigDecimal calculateCost(LocalDateTime entry, LocalDateTime exit) {
+        long minutes = entry.until(exit, ChronoUnit.MINUTES);
+        double total = 0.0;
+
+        if (minutes <= 15) {
+
+            // complete com a lógica para calcular o custo até 15 minutos de uso
+            total = PRIMEIROS_15_MINUTES;
+
+        } else if (minutes <= 60) {
+            total = PRIMEIROS_60_MINUTES;
+
+            // complete com a lógica para calcular o custo até os primeiros 60 minutos de uso
+
+        } else {
+            long addicionalMinutes = minutes - 60;
+            Double totalParts = ((double) addicionalMinutes / 15);
+            if (totalParts > totalParts.intValue()) {
+                total += PRIMEIROS_60_MINUTES + (ADICIONAL_15_MINUTES * (totalParts.intValue() + 1));
+            } else {
+                total += PRIMEIROS_60_MINUTES + (ADICIONAL_15_MINUTES * totalParts.intValue());
+            }
+        }
+
+        return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN);
     }
 }
