54730a9a8486ce24c6e5f908045b5dd52f20dc85
Lucas Lima <lucaserolima@gmail.com>
2024-02-15 18:41:54 -0400

    Test  ADD - getParking_ClientLogged_ReturnStatus200()

--- src/test/java/br/com/ero/demoparkapi/ParkingIT.java
+++ src/test/java/br/com/ero/demoparkapi/ParkingIT.java
@@ -290,6 +290,38 @@ public class ParkingIT {
 
 
     @Test
+    public void getParking_ClientLogged_ReturnStatus200(){
+        PageableDto responseBody = testClient
+                .get()
+                .uri("/api/v1/parking?size=1&page=0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "bia@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getContent().size()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getTotalPages()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getNumber()).isEqualTo(0);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getSize()).isEqualTo(1);
+
+        responseBody = testClient
+                .get()
+                .uri("/api/v1/parking?size=1&page=1")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "bia@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getContent().size()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getTotalPages()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getNumber()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getSize()).isEqualTo(1);
+    }
+    @Test
     public void getParking_ClientLoggedWithAdmin_profile_ReturnErrorStatus403() {
         testClient
                 .get()
--- src/test/resources/sql/parking/parking-insert.sql
+++ src/test/resources/sql/parking/parking-insert.sql
@@ -12,5 +12,5 @@ insert into parking_spot(id, code_parking_spot, status) values (400, 'A-04', 'FR
 insert into parking_spot(id, code_parking_spot, status) values (500, 'A-05', 'FREE');
 
 insert into clients_parking_spot(number_receipt, plate, brand, model, color, entry_date, id_client, id_parking_spot) values ('20240118-205706', 'FIT-1020', 'FIAT', 'PALIO', 'GREEN', '2024-01-18 20:57:06', 21, 100);
-insert into clients_parking_spot(number_receipt, plate, brand, model, color, entry_date, id_client, id_parking_spot) values ('20240119-205706', 'SIE-1020', 'HONDA', 'SIENA', 'WHITE', '2024-01-19 20:57:06', 22, 200);
+insert into clients_parking_spot(number_receipt, plate, brand, model, color, entry_date, id_client, id_parking_spot) values ('20240119-205706', 'SIE-1020', 'HONDA', 'SIENA', 'WHITE', '2024-01-19 20:57:06', 21, 200);
 insert into clients_parking_spot(number_receipt, plate, brand, model, color, entry_date, id_client, id_parking_spot) values ('20240120-205706', 'FIT-1020', 'FIAT', 'PALIO', 'GREEN', '2024-01-20 20:57:06', 22, 300);
